1bd01967cde41734b19f9bd5211a6a1f80813e6d
Josiah Aubin <josiahaubin@gmail.com>
2019-08-29 22:08:38 -0600

    Api pokemon drawing to doc

--- app/Controllers/PokeController.js
+++ app/Controllers/PokeController.js
@@ -3,13 +3,23 @@ import PokeService from "../Services/PokeService.js";
 //Private
 let _pokeService = new PokeService()
 
+function _drawAllApiPokemon() {
+    let template = `<ul>`
+    let apiPoke = _pokeService.apiPokemon
+
+    apiPoke.forEach(p => {
+        template += `<li>${p.name}</li>`
+    })
+
+    document.getElementById('api-pokemon').innerHTML = template + `</ul>`
+}
 
 
 //Public
 export default class PokeController {
     constructor() {
         //NOTE Register all subscribers
-
+        _pokeService.addSubscriber("apiPokemon", _drawAllApiPokemon)
 
         //NOTE Retrieve data
         _pokeService.getAllApiPokemon()
